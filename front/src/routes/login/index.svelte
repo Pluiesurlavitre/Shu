f20990773e6b44e8262bce82169186101cf2cab8
t.rollet <t.rollet@qwant.com>
2020-03-04 01:00:05 +0100

    wip authentication on front

---
+++ front/src/routes/login/index.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    import api from "api"
+    import { goto } from '@sapper/app'
+
+    import { session } from "store"
+
+    async function submit(e) {
+
+        await api.get('/airlock/csrf-cookie')
+
+        const res = await api.post('/login', { email, password })
+        console.log($session)
+
+        session.set(res.data.data)
+
+        goto('/')
+    }
+
+    let email = 'shu@example.com'
+    let password = 'password'
+</script>
+
+<form on:submit|preventDefault={submit}>
+
+    <input type="email" bind:value={email}>
+    <input type="password" bind:value={password}>
+    <button type="submit">Login</button>
+
+</form>
